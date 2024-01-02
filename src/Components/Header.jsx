72b225118cf20b91c801b2bf1959a1a8ce002b94
ShreyPurohit <purohitshrey9@gmail.com>
2024-01-03 00:46:56 +0530

    Day 1

---
+++ src/Components/Header.jsx
@@ -0,0 +1,58 @@
+import {AppBar, Toolbar, styled, InputBase, Box} from '@mui/material';
+import { Menu as MenuIcon, Search, Tune, HelpOutlined, SettingsOutlined, AppsOutlined, AccountCircleOutlined} from '@mui/icons-material';
+import { gmailLogo } from '../Constants/constant';
+
+const StyledAppBar = styled(AppBar)({
+    background: '#F5F5F5',
+    boxShadow: 'none'
+});
+
+const SearchWrapper = styled(Box)({
+    background: '#EAF1Fb',
+    marginLeft: 80,
+    borderRadius: 8,
+    minWidth: 690,
+    maxWidth: 720,
+    height: 48,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '0 20px',
+    '& > div':{
+        width: '100%',
+        padding: '0 10px'
+    }
+});
+
+const OptionsWrapper = styled(Box)({
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'end',
+    '& > svg':{
+        marginLeft: '20px'
+    }
+});
+
+const Header = ({toggleDrawer}) =>{
+    return(
+        <StyledAppBar position='static'>
+            <Toolbar>
+                <MenuIcon color='action' onClick={toggleDrawer}/>
+                <img src={gmailLogo} alt="Gmail Logo" style={{width: 110, marginLeft: 15}}/>
+                <SearchWrapper>
+                    <Search color='action' />
+                    <InputBase placeholder='Search Mail'/>
+                    <Tune color='action'/>
+                </SearchWrapper>
+                <OptionsWrapper>
+                    <HelpOutlined color='action'/>
+                    <SettingsOutlined color='action'/>
+                    <AppsOutlined color='action'/>
+                    <AccountCircleOutlined color='action' />
+                </OptionsWrapper>
+            </Toolbar>
+        </StyledAppBar>
+    );
+}
+
+export default Header;
---
+++ src/Components/SideBarContent.jsx
@@ -0,0 +1,44 @@
+import { Box, Button, List, ListItem, styled } from "@mui/material";
+import { CreateOutlined } from "@mui/icons-material";
+import { SIDEBAR_DATA } from "../Config/sidebar.config";
+
+const ComposedButton = styled(Button)({
+    background: '#c2e7ff',
+    color: '#001d35',
+    padding: 15,
+    borderRadius: 16,
+    minWidth: 140,
+    textTransform: 'none'
+});
+
+const Container = styled(Box)({
+    padding: 8,
+    '& > ul': {
+        padding: '10px 0 0 5px',
+        fontSize: '14px',
+        fontWeight: 500,
+        cursor: 'pointer'
+    },
+    '& > ul > li > svg': {
+        marginRight: 20
+    }
+});
+
+const SideBarContent = () =>{
+    return (       
+        <Container>
+            <ComposedButton><CreateOutlined />Compose</ComposedButton>
+            <List>
+                {
+                    SIDEBAR_DATA.map(data =>(
+                        <ListItem>
+                            <data.icon fontSize="small"/>{data.title}
+                        </ListItem>
+                    ))
+                }
+            </List>
+        </Container>
+    );
+}
+
+export default SideBarContent;
